6ef0bc1b15225d01fcdb8ab8b140db96c2d0856c
Sahara Panna <sahara.panna@gmail.com>
2020-01-16 21:13:38 -0500

    Immunizationtario

---
+++ Capstone2/app/src/main/java/ca/georgebrown/comp2074/capstone2/create_doctor.java
@@ -0,0 +1,14 @@
+package ca.georgebrown.comp2074.capstone2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class create_doctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_doctor);
+    }
+}
